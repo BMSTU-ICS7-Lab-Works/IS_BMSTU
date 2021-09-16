b05335344aa6efab2cbc684a2635a519c5fcb857
PSASP <andrey.psbk@gmail.com>
2021-09-17 00:29:37 +0300

    added gifs

--- lab1/program.py
+++ lab1/program.py
@@ -1,9 +1,38 @@
 from installator import check_keys
+import pyglet
 import sys
 
 if __name__ == '__main__':
     if not check_keys("license.key"):
-        print("You don't have a license!")
-        sys.exit()
+        # pick an animated gif file you have in the working directory
+        ag_file = "fail.gif"
+        animation = pyglet.resource.animation(ag_file)
+        sprite = pyglet.sprite.Sprite(animation)
+
+        # create a window and set it to the image size
+        win = pyglet.window.Window(width=sprite.width, height=sprite.height)
+
+
+        @win.event
+        def on_draw():
+            win.clear()
+            sprite.draw()
+
+
+        pyglet.app.run()
     else:
-        print('YUHUUU')
+        # pick an animated gif file you have in the working directory
+        ag_file = "cool.gif"
+        animation = pyglet.resource.animation(ag_file)
+        sprite = pyglet.sprite.Sprite(animation)
+
+        # create a window and set it to the image size
+        win = pyglet.window.Window(width=sprite.width, height=sprite.height)
+
+
+        @win.event
+        def on_draw():
+            win.clear()
+            sprite.draw()
+
+        pyglet.app.run()
