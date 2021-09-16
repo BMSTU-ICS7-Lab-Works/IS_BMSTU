9ad0481c26cccde37c97aafe5333f8fad9f6dbf8
PISAPSA <andrey.psbk@gmail.com>
2021-09-17 00:17:38 +0300

    added sys

--- lab1/program.py
+++ lab1/program.py
@@ -1,9 +1,9 @@
 from installator import check_keys
+import sys
 
 if __name__ == '__main__':
     if not check_keys("license.key"):
         print("You don't have a license!")
-        mb.showerror("error", "You don't have a license!")
         sys.exit()
     else:
         print('YUHUUU')
