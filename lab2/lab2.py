bde5f9a1cff4b3a88f4c47d459818e201bfce5e4
PSASP <andrey.psbk@gmail.com>
2021-10-14 22:54:58 +0300

    added binary read

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -10,10 +10,10 @@ def encrypt(word, rotor1, rotor2, rotor3):
     count2 = 0
     for el in word:
         curind = el
-        first_way = rotor3[rotor2.index(rotor1[ord(curind) - 65])]
+        first_way = rotor3[rotor2.index(rotor1[ord(curind)])]
         reflect = reflector[-reflector.index(first_way)+1]
         back_way = rotor1.index(rotor2[rotor3.index(reflect)])
-        res += chr(back_way + 65)
+        res += chr(back_way)
         rotor1 = rotate(rotor1, 1)
         count1 += 1
         if count1 % 26 == 0:
@@ -25,7 +25,33 @@ def encrypt(word, rotor1, rotor2, rotor3):
             rotor3 = rotate(rotor3, 1)
     return res
 
-symbols = [chr(i) for i in range(65, 91)]
+# шифруем/расшифровываем файл
+def encrypt_file(in_file, out_file, rotor1, rotor2, rotor3):
+    try:
+        input_file = open(in_file, 'rb')
+    except FileNotFoundError:
+        print("No such file! Try again!")
+        return
+
+    output_file = open(out_file, 'wb')
+    read_byte = input_file.read(1000)
+    file_text = ''
+
+    while read_byte:
+        byte_encrypt = b""
+        for s in read_byte:
+            file_text += chr(s)
+        file_text = encrypt(file_text, rotor1, rotor2, rotor3)
+        for ch in file_text:
+            byte_encrypt += bytes([ord(ch)])
+        output_file.write(byte_encrypt)
+        read_byte = input_file.read(1000)
+
+    input_file.close()
+    output_file.close()
+    return file_text
+
+symbols = [chr(i) for i in range(0, 255)]
 
 rotor1_copy = rotor1 = symbols[:]
 rotor2_copy = rotor2 = symbols[:]
@@ -38,17 +64,20 @@ random.shuffle(rotor3)
 reflector = symbols[:]
 random.shuffle(reflector)
 
-with open('text.txt', 'r',  encoding='utf-8') as f:
-    text = f.read()
-    print("Исходный текст:\n")
-    print(text)
-    print("\nЗашифрованный текст:\n")
-    with open('text_encrypt.txt', 'w',  encoding='utf-8') as ef:
-        textEncrypt = encrypt(text, rotor1, rotor2, rotor3)
-        ef.write(textEncrypt)
-        print(textEncrypt)
-        print("\nРасшифрованный текст:\n")
-        print(encrypt(textEncrypt, rotor1_copy, rotor2_copy, rotor3_copy))
+
+print("Enter file_name in: ")
+f_in = input()
+print("Enter file_name encrypt: ")
+f_out = input()
+print("Enter file_name result: ")
+f_res = input()
+text = encrypt_file(f_in, f_out, rotor1, rotor2, rotor3)
+print("\nЗашифрованный текст:\n")
+print(text)
+textEncrypt = encrypt_file(f_out, f_res, rotor1, rotor2, rotor3)
+
+print("\nРасшифрованный текст:\n")
+print(textEncrypt)
     
 
 
